22320ab79d8fb8c8eab2fd0d3e1cca5d126d5f3f
alagtari <alagtari@yahoo.fr>
2024-07-08 19:29:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const port = 3000;
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
