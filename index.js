7b50e56815363c31931fcc587515f395174fe453
alagtari <alagtari@yahoo.fr>
2024-07-08 20:05:43 +0100

    fd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-
+ 
 const port = 3000;
 const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
 
